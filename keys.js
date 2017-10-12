85ba8f398c60281740edcecb0598929392fdf0fc
unknown <joshuab171@yahoo.com>
2017-10-12 12:17:22 -0400

    first commit

---
+++ keys.js
@@ -0,0 +1,12 @@
+```JavaScript
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<qaFb3zEUr90treOT5UK2WjD3b>',
+  consumer_secret: '<7ugE7nzLxxkUchM0le1p9DxOKRnDq5fh0Jf6j3j8WQM4QmnArY>',
+  access_token_key: '<918486470826102785-tNCPV8abK9DobTMueA4tRxdYPcuqFeX>',
+  access_token_secret: '<882Y48KQ10qZHnAcR9vfwxBBBZtTEvv0vzSCGCXmr2NNu>',
+}
+
+module.exports = twitterKeys;
+```
